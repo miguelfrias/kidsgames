22ee7b2f769f55c58105580a90eafba1ca9fc5df
Miguel Frias <isc.miguelfrias@gmail.com>
2023-09-01 17:24:53 -0700

    make things bigger2

--- src/main.tsx
+++ src/main.tsx
@@ -33,7 +33,7 @@ const router = createBrowserRouter([
     ]
   },
 ], {
-  basename: import.meta.env.APP_ENV === 'prod' ? "/kidsgames" : ''
+  basename: import.meta.env.NODE_ENV === 'prod' ? "/kidsgames" : ''
 });
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.APP_ENV === 'prod' ? '/kidsgames/' : '',
+  base: process.env.NODE_ENV === 'prod' ? '/kidsgames/' : '',
   plugins: [react()],
 })
