7a7d7ea06222babfca09816fd1c38f4ac34cfb7c
Miguel Frias <isc.miguelfrias@gmail.com>
2023-10-05 18:26:58 -0700

    Add PWA support

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   // base: process.env.NODE_ENV !== 'local' ? '/kidsgames/' : '/',
-  plugins: [react()],
+  plugins: [
+    react({ tsDecorators: true, devTarget: "es2022" }),
+    VitePWA({
+      registerType: 'autoUpdate', // this will auto update immediately when changes are found
+      workbox: {
+        // workbox options for generateSW
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+      }
+    }),
+  ],
 })
