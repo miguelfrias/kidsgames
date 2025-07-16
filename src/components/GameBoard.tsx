62c726f223bfe47cc449f0a401b857d6a55c33b2
Miguel Frias <isc.miguelfrias@gmail.com>
2025-07-15 17:52:54 -0700

    test removing height

--- src/components/GameBoard.tsx
+++ src/components/GameBoard.tsx
@@ -125,7 +125,7 @@ function GameBoard({ word, onWordComplete, onBack, onNextWord }: GameBoardProps)
       onDragStart={handleDragStart}
       onDragEnd={handleDragEnd}
     >
-      <div className="min-h-[calc(100vh-4rem)] bg-gradient-to-b from-purple-100 to-pink-100 p-4">
+      <div className=" bg-gradient-to-b from-purple-100 to-pink-100 p-4">
       <div className="max-w-4xl mx-auto">
         {/* Header */}
         <div className="flex justify-between items-center mb-6">
